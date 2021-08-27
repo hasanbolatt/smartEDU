ca24252f5dbc50eeb37552d6e1741de943204f18
hasanbolatt <87964820+hasanbolatt@users.noreply.github.com>
2021-08-27 12:02:05 +0300

    27.08 ilk düzenleme

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 
-const pageRoute = require('./routes/pageRoute');
+const pageRoute = require('./routes/pageRoute');  
 const courseRoute = require('./routes/courseRoute');
-
+const categoryRoute = require('./routes/categoryRoute');
 
 const app = express();
 
@@ -28,6 +28,7 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 //Routes
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
+app.use('/categories', categoryRoute);
 
 
 const port = 3000;
--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,5 +1,7 @@
 const Course = require('../models/Course');
+const Category = require('../models/Category');
 
+//Ekleme
 exports.createCourse = async (req, res) => {
   try {
   const course = await Course.create(req.body);
@@ -12,17 +14,49 @@ exports.createCourse = async (req, res) => {
     res.status(400).json({
       status: 'fail',
       error,
+     });
+  }
+}
+
+//Kurlsarı listeleme
+exports.getAllCourses = async (req, res) => {
+  try {
+
+  const categorySlug = req.query.categories;
+  const category = await Category.findOne({slug:categorySlug});
+
+  let filter = {};
+
+  if(categorySlug)
+  {
+    filter = {category:category._id}
+  }
+
+  const courses = await Course.find(filter);
+  
+  const categories = await Category.find();  
+
+    res.status(200).render('courses',{
+      courses,
+      categories,
+      pageName: 'courses',
+    })
+  } catch(error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
     });
   }
 };
 
-exports.getAllCourses = async (req, res) => {
+//Tek kurs için ayrı sayfada açma
+exports.getCourse = async (req, res) => {
   try {
-  const courses = await Course.find();
+  const courses = await Course.findOne({slug:req.params.slug});
   
-    res.status(200).render('courses',{
+    res.status(200).render('course',{
       courses,
-      pageName: 'courses',
+      pageName: 'course',
     })
   } catch(error) {
     res.status(400).json({
